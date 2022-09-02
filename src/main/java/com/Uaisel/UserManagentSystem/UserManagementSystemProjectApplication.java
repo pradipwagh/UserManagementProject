c95f29f5ff5dc216c348bbdbb8f181d51a6983eb
pradipwagh <pradeepwagh@9561gmail.com>
2022-09-02 23:34:24 +0530

    US-1 Create Table in The Database

---
+++ src/main/java/com/Uaisel/UserManagentSystem/UserManagementSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Uaisel.UserManagentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementSystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementSystemProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Uaisel/UserManagentSystem/UserManagementSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Uaisel.UserManagentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSystemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
